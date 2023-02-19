fb0dc14a5f8018b56af2f60fe72a27665817c239
Estuardo Villeda <estuardovilleda2001@gmail.com>
2023-02-19 07:13:20 -0600

    feat: add login and register html templates and controller

---
+++ HospitalSanJose/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalSanJose.Controllers
+{
+    public class AuthController : Controller
+    {
+      
+
+        public IActionResult Login()
+        {
+            return View("Login/Index");
+        }
+
+        public IActionResult Register()
+        {
+            return View("Register/Index");
+        }
+    }
+}
